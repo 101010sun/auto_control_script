ef2c315c0ee05653736b31cedecd636817c2b6e0
611410066 <10sun19991119@gmail.com>
2023-12-18 23:01:40 +0800

    feat: update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from cellphone import SamsungS20FEController, OppoReno7Controller
 from computer import AcerT9300Controller
-from computer.computer_controller_asusa550v import AsusA550VController
+from computer import AsusA550VController
+from computer import MsiM16Controller
 from logger import Logger
 import random
 
@@ -113,28 +114,58 @@ def asusA550v(scenario: int):
     return
 
 
-def main():
-    scenario_list = [1, 2, 3, 4, 5, 6, 7, 8, 9] # 總共9個場景
-    
-    random_index = random.randint(0, len(scenario_list)-1)
-    value = scenario_list[random_index]
-    samsungS20FE(value)
-    scenario_list.remove(value)
-    
-    random_index = random.randint(0, len(scenario_list)-1)
-    value = scenario_list[random_index]
-    oppoReno7(value)
-    scenario_list.remove(value)
-    
-    random_index = random.randint(0, len(scenario_list)-1)
-    value = scenario_list[random_index]
-    acerT9300(value)
-    scenario_list.remove(value)
+def msiM16(scenario: int):
+    controller = MsiM16Controller()
+    controller.get_cursor_position()
+    # controller.enable_wifi()
 
-    random_index = random.randint(0, len(scenario_list)-1)
-    value = scenario_list[random_index]
-    asusA550v(value)
-    scenario_list.remove(value)
+    # if scenario == 1:
+    #     controller.watch_predefined_youtube_videos(10)
+    # elif scenario == 2:
+    #     controller.download_web_file()
+    # elif scenario == 3:
+    #     controller.play_spotify_music(5)
+    # elif scenario == 4:
+    #     controller.join_google_meet()
+    # elif scenario == 5:
+    #     controller.send_gmail()
+    # elif scenario == 6:
+    #     controller.view_specific_webpages(5)
+    # elif scenario == 7:
+    #     controller.upload_google_drive_file()
+    # elif scenario == 8:
+    #     controller.download_google_drive_file()
+    # elif scenario == 9:
+    #     controller.start_skype_call(5, 'msiM16_tester')
+    
+    # controller.disable_wifi()
+    return
+
+
+def main():
+    # scenario_list = [1, 2, 3, 4, 5, 6, 7, 8, 9] # 總共9個場景
+    
+    # random_index = random.randint(0, len(scenario_list)-1)
+    # value = scenario_list[random_index]
+    # samsungS20FE(value)
+    # scenario_list.remove(value)
+    
+    # random_index = random.randint(0, len(scenario_list)-1)
+    # value = scenario_list[random_index]
+    # oppoReno7(value)
+    # scenario_list.remove(value)
+    
+    # random_index = random.randint(0, len(scenario_list)-1)
+    # value = scenario_list[random_index]
+    # acerT9300(value)
+    # scenario_list.remove(value)
+
+    # random_index = random.randint(0, len(scenario_list)-1)
+    # value = scenario_list[random_index]
+    # asusA550v(value)
+    # scenario_list.remove(value)
+
+    msiM16(1)
 
 
 if __name__ == "__main__":
