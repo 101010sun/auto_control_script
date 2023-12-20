832ff447466f1a2f6f3296c08cd7377f89116016
101010sun <10sun19991119@gmail.com>
2023-12-20 15:18:43 +0800

    feat: update

---
+++ test_get_mouse.py
@@ -0,0 +1,16 @@
+from base.logger import Logger
+import time
+import pyautogui
+
+def get_cursor_position():
+        time.sleep(2)
+        print(pyautogui.position())
+        return 
+
+def main():
+    get_cursor_position()
+
+
+if __name__ == "__main__":
+    logger = Logger('WPA3DataSet', 'main')
+    main()
--- test_msi.py
+++ test_msi.py
@@ -4,11 +4,6 @@ import time
 import pyautogui
 import json
 
-def get_cursor_position():
-        time.sleep(2)
-        print(pyautogui.position())
-        return 
-
 def test_youtube():
     youtube_video_list = []
     with open("url_list.json", "r") as f:
@@ -40,15 +35,15 @@ def test_download_web_file():
     random_index = random.randint(0, len(web_download_list)-1)
 
     pyautogui.click(10, 15, duration=0.5)  # 選單
-    pyautogui.click(84, 53, duration=0.5)  # 選單搜尋
+    pyautogui.click(84, 53, duration=0.3)  # 選單搜尋
     pyautogui.typewrite("Google Chrome")  # 開啟 chrome
     pyautogui.press('enter')
-    time.sleep(2)
+    time.sleep(1)
 
-    pyautogui.click(172, 96, duration=0.5)  # 網址列
+    pyautogui.click(172, 96, duration=0.3)  # 網址列
     pyautogui.typewrite(web_download_list[random_index])
     pyautogui.press('enter')
-    time.sleep(2)
+    time.sleep(1)
     time.sleep(25)
 
     pyautogui.click(951, 574, duration=0.5)  # 移至筆電中心
