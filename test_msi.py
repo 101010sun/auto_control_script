f6d42deb9a80028995960007bc4da8422769015d
101010sun <10sun19991119@gmail.com>
2023-12-20 14:46:29 +0800

    feat: update

--- test_msi.py
+++ test_msi.py
@@ -16,23 +16,19 @@ def test_youtube():
         youtube_video_list = file["youtube_video_list"]
 
     random_index = random.randint(0, len(youtube_video_list)-1)
-    pyautogui.moveTo(10, 15, interval=1)
-    pyautogui.click(10, 15)  # 選單
-    pyautogui.moveTo(84, 53, interval=1)
-    pyautogui.click(84, 53)  # 選單搜尋
+    pyautogui.click(10, 15, duration=1)  # 選單
+    pyautogui.click(84, 53, duration=1)  # 選單搜尋
     pyautogui.typewrite("Google Chrome")  # 開啟 chrome
     pyautogui.press('enter')
     time.sleep(2)
 
-    pyautogui.moveTo(172, 96, interval=1)
-    pyautogui.click(172, 96)  # 網址列
+    pyautogui.click(172, 96, duration=1)  # 網址列
     pyautogui.typewrite(youtube_video_list[random_index])
     pyautogui.press('enter')
     time.sleep(2)
     time.sleep(10) # play time
 
-    pyautogui.moveTo(951, 574, interval=1)
-    pyautogui.click(951, 574)  # 移至筆電中心
+    pyautogui.click(951, 574, duration=1)  # 移至筆電中心
     pyautogui.hotkey('alt', 'f4', interval=1)  # 關閉視窗
 
     return
